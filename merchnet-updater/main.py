cbcc9cfc83583a7f0704fdb9f5ed55a8fafd9065
Edidiong Akpan <100961974+mredidy@users.noreply.github.com>
2025-04-14 00:08:40 +0100

    Update main.py

--- merchnet-updater/main.py
+++ merchnet-updater/main.py
@@ -140,4 +140,4 @@ def run_cron_job():
         return f"❌ Cron error: {str(e)}", 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
